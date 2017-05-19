f8ca34472aaf5896f95fc60fefa2b82201a259cf
ColeDeanShepherd <cole@coledeanshepherd.com>
2017-05-18 21:27:28 -0500

    Added social sharing links, JS obsfucator, refactored build. improved styling.

--- build/build-utils.ts
+++ build/build-utils.ts
@@ -1,20 +1,32 @@
 declare function require(name: string): any;
 const fs = require('fs-extra');
+const execSync = require('child_process').execSync;
 
-function generateHtmlDocument(title: string, contents: string): string {
+function generateHtmlDocument(page: any, contents: string): string {
   const metaTags = `<meta charset="UTF-8" />`;
-  const titleTag = `<title>${title} - WitSpur</title>`;
+  const titleTag = `<title>${page.name} - WitSpur</title>`;
   const stylesheetLinks = `<link rel="stylesheet" href="./normalize.css"><link rel="stylesheet" href="./global.css">`;
   const googleAnalyticsTag = `<script> (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){ (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) })(window,document,'script','https://www.google-analytics.com/analytics.js','ga'); ga('create', 'UA-72494315-4', 'auto'); ga('send', 'pageview'); </script>`;
   const headTag = `<head>${metaTags}${titleTag}${stylesheetLinks}${googleAnalyticsTag}</head>`;
 
   const logoTag = `<div class="logo"><a href="/">WitSpur</a></div>`;
-  const headerTag = `<div class="header${(title === "Home") ? " front-page" : ""}">${logoTag}</div>`;
-  const subHeaderTag = `<div class="sub-header${(title === "Home") ? " front-page" : ""}">Interactive tools and media to inspire your mind.</div>`;
+
+  const absolutePageUrl = `http://witspur.com${(page.outputFileName !== 'index') ? `/${page.outputFileName}.html` : ""}`;
+  
+  const facebookShareUrl = `https://www.facebook.com/sharer/sharer.php?u=${encodeURI(absolutePageUrl)}`;
+  const facebookShareLi = `<li><a href="${facebookShareUrl}" target="_blank"><img src="img/facebook.svg" alt="Share on Facebook" /></a></li>`;
+  
+  const twitterShareUrl = `https://twitter.com/intent/tweet?source=${encodeURI(absolutePageUrl)}&text=${encodeURI(absolutePageUrl)}&via=WitSpur`;
+  const twitterShareLi = `<li><a href="${twitterShareUrl}" target="_blank"><img src="img/twitter.svg" alt="Tweet" /></a></li>`;
+  
+  const socialSharingLinks = `<ul class="social-sharing-icons">${facebookShareLi}${twitterShareLi}</ul>`;
+
+  const subHeaderTag = `<div class="sub-header${(page.name === "Home") ? " front-page" : ""}">Interactive tools and media to inspire your mind.</div>`;
+  const headerTag = `<div class="header${(page.name === "Home") ? " front-page" : ""}">${logoTag}${subHeaderTag}${socialSharingLinks}</div>`;
   const footerTag = `<div class="footer"></div>`;
 
   const scriptTags = `<script src="./bundle.js"></script>`;
-  const bodyTag = `<body>${headerTag}${subHeaderTag}${contents}${footerTag}${scriptTags}</body>`;
+  const bodyTag = `<body>${headerTag}${contents}${footerTag}${scriptTags}</body>`;
 
   const htmlTag = `<html>${headTag}${bodyTag}</html>`;
 
@@ -35,11 +47,21 @@ export function buildHTML() {
 
   pages.forEach(page => {
     const contents = fs.readFileSync(`src/${page.contentsFileName}.html`);
-    const pageHtml = generateHtmlDocument(page.name, contents);
+    const pageHtml = generateHtmlDocument(page, contents);
     fs.writeFileSync(`dist/${page.outputFileName}.html`, pageHtml);
   });
 }
 
 export function buildImages() {
   fs.copySync('img', 'dist/img');
+}
+
+export function buildJS(uglify: boolean) {
+  execSync('webpack');
+
+  if(uglify) {
+    fs.removeSync('dist/bundle.js.map');
+    execSync('uglifyjs dist/bundle.js --output dist/bundle.js');
+    //execSync('uglifyjs dist/bundle.js --source-map --output dist/bundle.js');
+  }
 }
--- build/rebuild.ts
+++ build/rebuild.ts
@@ -1,6 +1,7 @@
-import { clean, buildHTML, buildCSS, buildImages } from "./build-utils";
+import { clean, buildHTML, buildCSS, buildImages, buildJS } from "./build-utils";
 
 clean();
 buildHTML();
 buildCSS();
 buildImages();
+buildJS(true);
